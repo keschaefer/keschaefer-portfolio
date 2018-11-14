c500e12171a22d3cd270c66843628d0f76b03322
keschaefer <keschaefer@gmail.com>
2018-11-14 14:59:41 -0700

    added content to title component

--- src/Components/Title.js
+++ src/Components/Title.js
@@ -4,7 +4,10 @@ const Title = () => {
    return (
       <div>
          <h1>Kate Schaefer</h1>
-         <h1>Full Stack Web Developer</h1>
+         <h2>Full Stack Web Developer</h2>
+         <div className= "profile-image">
+            <img src= "https://dl.dropboxusercontent.com/s/6h2za71h8tlptpn/alley-headshot-hires.jpg" alt= "personal profile image headshot"></img>
+         </div>
       </div>   
    )
 }
