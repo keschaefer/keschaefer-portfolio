757f9281915465af640528d45cd6943d8a406ca7
keschaefer <keschaefer@gmail.com>
2018-11-14 15:26:48 -0700

    changed Projects to Work

---
+++ src/Components/Work.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import IndProject from './IndProject.js';
+
+const Work = () => {
+   return (
+      <div>
+         <h1>Work</h1>
+         <IndProject/>
+      </div>   
+   )
+}
+export default Work
