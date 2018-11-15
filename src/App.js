fa9017c37e39cac80babdead6de543cc73fca17d
keschaefer <keschaefer@gmail.com>
2018-11-15 08:59:03 -0700

    populated state with project data

--- src/App.js
+++ src/App.js
@@ -7,6 +7,46 @@ import Footer from './Components/Footer.js';
 import Title from './Components/Title.js';
 
 class App extends Component {
+  constructor() {
+    super()
+      this.state = {
+        projects: [
+          {
+          "title": "Denver Doggie",
+          "description": "Site that clues Denver dog owners in on where their pup is welcome",
+          "tech-used": "Express, Node.js, JavaScript, React",
+          "image": "https://snag.gy/kFOJwG.jpg",
+          "git-hub": "https://github.com/keschaefer/denver-doggie",
+          "live-site":,
+        },
+        {
+          "title": "g-school Inbox",
+          "description": "Basic gmail-style inbox utilizing a school provided API",
+          "tech-used": "React, JavaScript, AJAX, Bootstrap",
+          "image": "https://snag.gy/Nhwdf5.jpg",
+          "git-hub": "https://github.com/keschaefer/inbox-react-app",
+          "live-site":"http://inbox-app-gschool.surge.sh/",
+        },
+        {
+          "title": "g-Food Delivers" ,
+          "description": "Basic online food ordering App.",
+          "tech-used": "Javascript, AJAX, HTML, CSS",
+          "image": "https://snag.gy/tBCreg.jpg",
+          "git-hub": "https://github.com/keschaefer/g-food-Delivers",
+          "live-site": "http://g-foodeats-keschaefer.surge.sh/",
+        },
+        {
+          "title": "Pixel Art Maker",
+          "description": "Create your own pixel art!",
+          "tech-used": "Javascript, HTML, CSS" ,
+          "image": "https://snag.gy/XAzGRQ.jpg",
+          "git-hub": "https://github.com/keschaefer/pixel-art-maker-public",
+          "live-site": "http://keschaefer-pixel-art.surge.sh/"
+        },
+      ]
+      }
+  }
+  
   render() {
     return (
       <div className= "App">
