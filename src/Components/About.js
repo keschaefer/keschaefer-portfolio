0c60fdd0c7da024235d87e436b6f2b44a26df8d6
keschaefer <keschaefer@gmail.com>
2018-11-29 11:22:44 -0700

    added styling to About section, updated with Tech Skills

--- src/Components/About.js
+++ src/Components/About.js
@@ -6,17 +6,39 @@ const About = () => {
          <h1>About</h1>
          <div className= "background">
             <h2>Background</h2>
-            <p>I am a current student in the Web Development Immersive program at Galvanize Denver. After years of plotting my escape from the world of professional fundraising events, I am so pleased to have begun this journey into the world of web development and am actively seeking an entry-level position using my new skills.
-
+            <p>I am a current student in the Web Development Immersive program at Galvanize Denver. After years of plotting my escape from the world of professional fundraising events, I am so pleased to have begun this journey into the world of web development and am actively seeking an entry-level position using my new skills. </p>
+            <p>
             I fill my free time playing in the great outdoors including climbing, hiking, and camping. I love to travel and have zig-zagged my way across Europe, lived in Spain and Scotland, but also enjoy a good old Denver staycation.</p>
          </div>
          <div className= "desired-org">
             <h2>What I'm Looking For</h2>
             <p>I got into coding for both the intellectual challenge and the opportunity to drive change on a grander scale than my previous, local work, in non-profits. I am highly detail-focused and love the precision that good code requires. I am looking to work at a small-mid sized company that focuses on a triple bottom line. Working on a project that combats climate change, has an outdoor industry connection, or just generally does good in the world would be a dream.</p> 
          </div>
-         <div className= "why-me">
-            <h2>Why Kate?</h2>
-            <p>Why choose me to be part of your engineering team? Quite simply I am....</p>
+         <div className= "tech-stack">
+            <h2>My Tech Stack</h2>
+            <div className= "tech-list">
+            <ul>
+               <li>JavaScript</li>
+               <li>CSS</li>
+               <li>HTML</li>
+               <li>React</li>
+               <li>React Router</li>
+               <li>Bootstrap</li>
+            </ul>
+            <ul> 
+               <li>Express</li>
+               <li>Node.js</li>
+               <li>Knex</li>
+               <li>AJAX</li>
+               <li>Heroku</li>
+               <li>PostgreSQL</li>
+            </ul>
+            </div>
+         </div>
+         <div className= "resume-button">
+            <button>
+               <a href="https://docs.google.com/document/d/1XEcSTucUSt4IdD3dlvQhgCpjOIHgb63DG8CEdQ0z65Q/export?format=pdf">Download Resume</a>
+               </button>
          </div>
       </div>
    )
