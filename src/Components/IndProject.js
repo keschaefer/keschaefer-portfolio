e4849e2b646738d2bccc4e58aa0ecf36e0274c0d
keschaefer <keschaefer@gmail.com>
2019-01-02 11:10:12 -0700

    added flexbox to card body to fix alignment issues

--- src/Components/IndProject.js
+++ src/Components/IndProject.js
@@ -4,12 +4,14 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 const IndProject = (props) => {
    return props.projects.map(project => {
          return(
-         <div className= "card ind-project" key= {project.title}>
+         <div className= "card ind-project rounded" key= {project.title}>
             <p className= "project-title">{project.title}</p>
             <img className= "project-image card-img-top" src= {project.image} alt= "snapshot of live website for project"></img>
             <div className= "card-body">
-               <p className= "card-text">{project.description}</p>
-               <p className= "tech-used">{project.techused}</p>
+               <div className= "card-text">
+                  <p className= "card-description">{project.description}</p>
+                  <p className= "tech-used">{project.techused}</p>
+               </div>
                <div className= "links">
                   <button className= "live-site-button"><a target="_blank" rel="noopener noreferrer" href={project.livesite}>Visit {project.title}</a></button>
                   <a href= {project.github} target="_blank" rel="noopener noreferrer"><FontAwesomeIcon class= "card-icon" icon={['fab', 'github']}/></a>
